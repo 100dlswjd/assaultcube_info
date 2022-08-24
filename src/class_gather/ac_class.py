b08a3970226d643613f811b1d8302b2dcd7f3b9e
100dlswjd <100dlswjd@naver.com>
2022-08-24 13:56:11 +0900

    undead 추가

--- src/class_gather/ac_class.py
+++ src/class_gather/ac_class.py
@@ -5,6 +5,16 @@ class AC(ProcessMemoryControl):
         super().__init__("ac_client")
         self.process_base_addr = super().base_addr()
     
+    def undead_on(self):
+        nop_code = b'\x90\x90\x90'
+        target_addr = self.process_base_addr + 0x1c223
+        super().write(target_addr, nop_code)
+
+    def undead_off(self):
+        org_code = b')s\x04'
+        target_addr = self.process_base_addr + 0x1c223
+        super().write(target_addr, org_code)
+    
     def hp(self):
         HP_OFFSET = 0xEC
         pointer_start = super().read_dword(self.process_base_addr + 0x0017E0A8)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,4 @@
 from class_gather.ac_class import AC
 import time
 a = AC()
-
-while True:
-    hp = a.hp()
-    bullet = a.bullet()
-
-    print("체력 : ",hp)
-    print("총알 : ",bullet)
-    print("-"*99)
-    time.sleep(0.5)
+a.undead_on()
