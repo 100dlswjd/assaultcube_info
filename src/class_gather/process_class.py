5541df4e1f99d05591385423edeca8976173ad2d
100dlswjd <100dlswjd@naver.com>
2022-08-26 03:15:24 +0900

    read_float 함수 추가

--- src/class_gather/process_class.py
+++ src/class_gather/process_class.py
@@ -2,6 +2,7 @@ import win32process
 import win32api
 import win32con
 import struct
+
 from tool import addrtool
 
 class ProcessMemoryControl:
@@ -13,21 +14,23 @@ class ProcessMemoryControl:
 
     def __init__(self, process_name : str):
         self.name = process_name
-        self.pid = addrtool.Getpid(process_name)
+        self.pid = addrtool.getpid(process_name)
         self.process_base_addr = addrtool.get_base_addr(process_name)
         self.process = self.OpenProcess(self.PROCESS_ALL_ACCESS, False, self.pid)
     
     def base_addr(self):
         return self.process_base_addr
 
-    def read(self, addr : int, read_bytes : int, types : str = "int") -> int or float:
+    def read(self, addr : int, read_bytes : int) -> int:
         read_data : bytes = self.ReadProcessMemory(self.process, addr, read_bytes)
-        if types == "int":
-            read_data = int.from_bytes(read_data, "little")
-            return read_data
-        elif types == "float":
-            read_data = struct.unpack('f', read_data)
-            return read_data[0]
+        read_data = int.from_bytes(read_data, "little")
+        return read_data
+    
+    def read_float(self, addr : int):
+        read_data = self.read(addr, 4)
+        read_data = read_data.to_bytes(4, 'little')
+        read_data = struct.unpack('f', read_data)
+        return read_data[0]
 
     def read_byte(self, addr : int):
         return self.read(addr, 1)
@@ -35,15 +38,13 @@ class ProcessMemoryControl:
     def read_word(self, addr : int):
         return self.read(addr, 2)
 
-    def read_dword(self, addr : int, types : str = "int"):
-         return self.read(addr, 4, types)
+    def read_dword(self, addr : int):
+        return self.read(addr, 4)
 
     def write(self, addr : int, write_data : bytes) -> bool:
-        flag = self.WritePocessMemory(self.process, addr, write_data)
-        if flag:
-            return True
-        else:
-            return False
+        result = self.WritePocessMemory(self.process, addr, write_data)
+        return bool(result)
+        # return True if result == 2 else False
 
     def close(self):
         self.CloseHandle(self.process)
