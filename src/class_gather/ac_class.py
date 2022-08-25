4bf298592b7111dbe04ea9e9607b1967b510f906
100dlswjd <100dlswjd@naver.com>
2022-08-26 03:14:39 +0900

    변경하지 않는 값 초기화, 상수는 제일 위로 뺌

--- src/class_gather/ac_class.py
+++ src/class_gather/ac_class.py
@@ -1,43 +1,52 @@
 from class_gather.process_class import ProcessMemoryControl
 
 class AC(ProcessMemoryControl):
+    HP_OFFSET = 0xEC
+    BEULLET_OFFSET = 0x140
+
     def __init__(self):
         super().__init__("ac_client")
         self.process_base_addr = super().base_addr()
+        self.x_pos_addr = self.process_base_addr + 0x17E090
+        self.y_pos_addr = self.process_base_addr + 0x17E094
+        self.z_pos_addr = self.process_base_addr + 0x17E098
+        self.undead_addr = self.process_base_addr + 0x1c223
+        self.hp_addr = self.process_base_addr + 0x0017E0A8
+        self.bullet_addr = self.process_base_addr + 0x0018AC00
     
     def x_pos(self):
-        target_addr = self.process_base_addr + 0x17E090
-        x_pos = super().read_dword(target_addr, "float")
-        return x_pos
+        return super().read_float(self.x_pos_addr)
 
     def y_pos(self):
-        target_addr = self.process_base_addr + 0x17E094
-        y_pos = super().read_dword(target_addr, "float")
-        return y_pos
-
+        return super().read_float(self.y_pos_addr)
+        
     def z_pos(self):
-        target_addr = self.process_base_addr + 0x17E098
-        z_pos = super().read_dword(target_addr, "float")
-        return z_pos
+        return super().read_float(self.z_pos_addr)
 
     def undead_on(self):
         nop_code = b'\x90\x90\x90'
-        target_addr = self.process_base_addr + 0x1c223
-        super().write(target_addr, nop_code)
+        super().write(self.undead_addr, nop_code)
 
     def undead_off(self):
         org_code = b')s\x04'
-        target_addr = self.process_base_addr + 0x1c223
-        super().write(target_addr, org_code)
+        super().write(self.undead_addr, org_code)
     
     def hp(self):
-        HP_OFFSET = 0xEC
-        pointer_start = super().read_dword(self.process_base_addr + 0x0017E0A8)
-        hp = super().read_word(pointer_start + HP_OFFSET)
+        pointer_start = super().read_dword(self.hp_addr)
+        hp = super().read_word(pointer_start + self.HP_OFFSET)
         return hp
+    
+    def hp_date(self):
+        hp = 1000
+        pointer_start = super().read_dword(self.hp_addr)
+        super().write(pointer_start + self.HP_OFFSET, hp.to_bytes(4, "little"))
 
     def bullet(self):
-        BEULLET_OFFSET = 0x140
-        pointer_start = super().read_dword(self.process_base_addr + 0x0018AC00)
-        bullet = super().read_dword(pointer_start + BEULLET_OFFSET)
+        pointer_start = super().read_dword(self.bullet_addr)
+        bullet = super().read_dword(pointer_start + self.BEULLET_OFFSET)
         return bullet
+
+    def bullet_update(self):
+        bullet = 500
+        pointer_start = super().read_dword(self.bullet_addr)
+        super().write(pointer_start + self.BEULLET_OFFSET, bullet.to_bytes(4, "little"))
